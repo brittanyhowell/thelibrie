a147d3eb3036481befd0cd10362f8b79aa36765b
Brittany <bh837982@gmail.com>
2021-01-23 19:02:02 +0000

    hello

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -17,7 +17,7 @@ class BlogPostTemplate extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
+        <h1 style={{color:'red'}}>{post.frontmatter.title}</h1>
         <p
           style={{
             ...scale(-1 / 5),
