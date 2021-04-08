734585c60c7c7f02181418a94f04286ffdff9293
Chris Donaldson <chris@digital-labs.co.uk>
2021-04-08 22:11:06 +0100

    librie

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,60 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import { Navigation } from './navigation.tsx'
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3>
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-        }}
-      >
-        {header}
+      <div >
+        <Navigation location={location} title={title} />
         {children}
-        <footer>
+        <footer className={"fixed bottom-0 bg-yellow-200 min-w-full"}>
+          <div className={"container mx-auto py-2"}>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </div>
         </footer>
       </div>
     )
